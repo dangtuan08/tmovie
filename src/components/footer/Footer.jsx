958a4718c9c38abf7002e571929ef54f855d7356
John Doe <tuanou17@gmail.com>
2022-06-07 16:06:18 +0700

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import './footer.scss'
+
+import {Link} from 'react-router-dom'
+
+import bg from '../../assets/footer-bg.jpg'
+import logo from '../../assets/tmovie.png';
 
 function Footer() {
-  return <div>Footer</div>;
+  return <div className="footer" style={{backgroundImage:`url(${bg})`}}>
+    <div className="footer__content">
+      <div className="footer__content--logo">
+        <div className="logo">
+        <img src={`${logo}`} alt="logo" />
+        <Link to={'/'}>tMovies</Link>
+        </div>
+        
+      </div>
+    </div>
+  </div>;
 }
 
 export default Footer;
