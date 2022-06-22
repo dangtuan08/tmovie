0261a294b288986e6152ddc59313cd188888f13f
dangtuan08 <tuanou17@gmail.com>
2022-06-22 16:23:41 +0700

    add loading detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
 
 import tmdbApi, { category, movieType } from "../api/tmdbApi";
 import Content from "../components/detail-layout/content/Content";
@@ -8,18 +9,21 @@ import MovieList from "../components/movie-list/MovieList";
 
 function Detail() {
   const [detailMovie, setDetailMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+  console.log(detailMovie);
   const params = useParams();
   // const [id, category] = params;
   // console.log(params);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-
+    setLoading(true);
     tmdbApi
       .detail(params.category, params.id, { params: {} })
       .then((res) => {
         // console.log(res);
         setDetailMovie(res);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -28,18 +32,26 @@ function Detail() {
 
   return (
     <>
-      <Content movie={detailMovie} />
-      <div className="container">
-        {/* <Video />
+      {loading ? (
+        <div className="loading">
+          <BeatLoader color={"#ff0000"} size={150} />
+        </div>
+      ) : (
+        <>
+          <Content movie={detailMovie} />
+          <div className="container">
+            {/* <Video />
         <Video />
         <Video />
         <Video /> */}
-        <MovieList
-          category={category.movie}
-          type={movieType.popular}
-          title="Trending Movies"
-        />
-      </div>
+            <MovieList
+              category={category.movie}
+              type={movieType.popular}
+              title="Trending Movies"
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
